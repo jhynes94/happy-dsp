3b5c8971d6c6811616894695126708846c73a67c
Justin Hynes-Bruell <jhynesbr@ltx-credence.com>
2017-09-21 17:15:23 -0400

    THD, THD+N, SNR

--- renderer.js
+++ renderer.js
@@ -14,15 +14,15 @@ var showTime = true;
 function UserException(message) {
     this.message = message;
     this.name = 'UserException';
- }
+}
 
 function showFreqDomain() {
     showFreq = !showFreq;
-    if(showFreq == true){
+    if (showFreq == true) {
         $('#showFreqDomain').text("Show Freq Domain")
         document.getElementById('spectrum').style.display = "none"; // Hide Landing Page
     }
-    if(showFreq == false){
+    if (showFreq == false) {
         $('#showFreqDomain').text("Hide Freq Domain")
         document.getElementById('spectrum').style.display = "inline"; // Hide Landing Page
     }
@@ -32,46 +32,52 @@ function showTimeDomain() {
 
 }
 
-function newData(){
-    const {dialog} = require('electron').remote;
-    
+function newData() {
+    const { dialog } = require('electron').remote;
+
     var path = dialog.showOpenDialog({
         filters: [
-            
-               { name: 'text', extensions: ['txt'] }
-            
-              ],
+
+            { name: 'text', extensions: ['txt'] }
+
+        ],
         properties: ['openFile']
     });
-   
+
     dataModels.push(new dataModel(path.toString()));
     console.log("New Data Model created");
 
 
     document.getElementById('DataSection').style.display = "inline"; //Show Data Page
     document.getElementById('LandingPage').style.display = "none"; // Hide Landing Page
-    
+
 
 }
 
 class dataModel {
 
     constructor(pathToFile) {
-        
+
         $('#FileOutput').text(pathToFile)
-        
+
         //public variables
         this.pathToFile = pathToFile;
         this.rawData = this.readDataIn(this.pathToFile);
         this.fftData = this.fft(this.rawData);
         this.fftDataMag = this.magDataDb(this.fftData);
         this.harmonics = this.calcHarmonics(this.fftDataMag);
-        this.THD = this.calcTHD(this.fftDataMag, this.harmonics);
+        this.THD = this.calcTHD(this.fftData, this.harmonics);
+        this.THDN = this.calcTHDN(this.fftData, this.harmonics[1]);
+        this.SNR = this.calcSNR(this.THDN, this.fftDataMag[this.harmonics[1]]);
         this.DC = this.fftDataMag[0];
+
+        console.log("Fund" + this.fftData[this.harmonics[1]])
 
 
         //Move to Controller along with harmonic data
         $('#THD').text(this.THD.toString())
+        $('#THDN').text(this.THDN.toString())
+        $('#SNR').text(this.SNR.toString())
         $('#DC').text(this.DC.toString())
 
 
@@ -79,48 +85,62 @@ class dataModel {
         this.plotData(this.rawData, 'timeDomain')
     }
 
-    readDataIn (pathToFile) {
+
+
+
+    A_WeightedFrequencies(waveform) {
+        //https://www.npmjs.com/package/a-weighting
+        var aWeight = require('a-weighting/a');
+        var ft = require('fourier-transform');
+        var frequencies = ft(waveform).map(function (magnitude, i, data) {
+            var frequency = 22050 * i / data;
+
+            return aWeight(frequency) * magnitude;
+        });
+        return (frequencies)
+    }
+
+    readDataIn(pathToFile) {
 
         const fs = require("fs");
-    
+
         var data = fs.readFileSync(pathToFile);
-        
+
         //Convert string of Data to Array of Floats
-        var arrayOfData = data.toString().split(/\r\n|\r|\n/g).map(function(i){
+        var arrayOfData = data.toString().split(/\r\n|\r|\n/g).map(function (i) {
             return parseFloat(i);
         })
 
         //Clean Array of floats to remove NaN error at last place
-        arrayOfData.splice(arrayOfData.length-1, 1)
+        arrayOfData.splice(arrayOfData.length - 1, 1)
 
-        return(arrayOfData);
+        return (arrayOfData);
     }
 
-    fft (rawData) {
+    fft(rawData) {
         var ft = require('fourier-transform');
         var db = require('decibels');
-    
+
         var spectrum = ft(rawData);
 
-        return(spectrum)
+        return (spectrum)
     }
 
-    magDataDb (fftData) {
+    magDataDb(fftData) {
         //convert to decibels 
         var db = require('decibels');
         var decibels = fftData.map((value) => db.fromGain(value))
 
-        return(decibels);
+        return (decibels);
     }
 
-    //TODO
-    calcHarmonics (fftDataArray) {
+    calcHarmonics(fftDataArray) {
 
-        //Find the Fundemental Freq
+        //Find the Fundemental Freq by searching for largest freq besides DC
         var largestValueIndex = -1;
         var largestValue = -200;
-        for (var i = 1; i < fftDataArray.length-1; i++) {
-            if(fftDataArray[i] > largestValue){
+        for (var i = 1; i < fftDataArray.length - 1; i++) {
+            if (fftDataArray[i] > largestValue) {
                 largestValue = fftDataArray[i];
                 largestValueIndex = i;
             }
@@ -130,81 +150,112 @@ class dataModel {
         var harmonicIndex = [];
         harmonicIndex.push(0);
         harmonicIndex.push(largestValueIndex);
-        
 
+
+        //Search for harmonic in range
         function findLargestValueIndexInRange(array, range, searchIndex) {
 
             //if Search index is greater than array, the harmonic is out of range
-            if(searchIndex+range > array.length){
-                return(-1);
+            if (searchIndex + range > array.length) {
+                return (-1);
             }
 
             var largestValueInRange = -200; //-Infinity
             var largestValueInRangeIndex = searchIndex;
-            for (var i = searchIndex-range; i <= searchIndex+range; i++) {
-                if(array[i] > largestValueInRange) {
+            for (var i = searchIndex - range; i <= searchIndex + range; i++) {
+                if (array[i] > largestValueInRange) {
                     largestValueInRange = array[i];
                     largestValueInRangeIndex = i;
                 }
             }
-            return(largestValueInRangeIndex);
+            return (largestValueInRangeIndex);
         }
 
 
 
         var numberOfHarmonics = 10
-        for(var i = 2; i <= numberOfHarmonics; i++){
-            var sampleHarmonic = findLargestValueIndexInRange(fftDataArray, 5, harmonicIndex[i-1]*i);
-            if(sampleHarmonic == -1){
+        for (var i = 2; i <= numberOfHarmonics; i++) {
+
+            var sampleHarmonic = findLargestValueIndexInRange(fftDataArray, 5, harmonicIndex[1] * i);
+
+            if (sampleHarmonic == -1) {
                 //quit for loop
-                i = numberOfHarmonics+1;
+                i = numberOfHarmonics + 1;
             }
             else {
                 harmonicIndex.push(sampleHarmonic);
             }
         }
 
-        for(var i = 1; i < harmonicIndex.length; i++){
+        for (var i = 1; i < harmonicIndex.length; i++) {
             console.log(i + ": " + harmonicIndex[i] + " " + fftDataArray[harmonicIndex[i]].toString())
             $('#' + i + 'Harmonic').text(fftDataArray[harmonicIndex[i]].toString())
         }
 
-        return(harmonicIndex);
+        return (harmonicIndex);
     }
 
-    //TODO
-    calcTHD (data, harmonicIndex) {
+    calcTHD(data, harmonicIndex) {
         var THD = -300;
 
-        
-        console.log(Math.pow(2,2));
-
         var HarmonicAmplitudesSquared = 0;
-        for(var i = 2; i < harmonicIndex.length; i++){
+        for (var i = 2; i < harmonicIndex.length; i++) {
             HarmonicAmplitudesSquared = HarmonicAmplitudesSquared + Math.pow(data[harmonicIndex[i]], 2)
         }
 
         THD = Math.sqrt(HarmonicAmplitudesSquared);
 
-        THD = THD/data[harmonicIndex[1]];
+        THD = THD / data[harmonicIndex[1]];
+
+        THD = 20 * Math.log10(THD);
 
 
-        if (THD == -300){
+        if (THD == -300) {
             console.log("THD Calculation Failed");
         }
-        else{
+        else {
             console.log("THD: " + THD);
-            return(THD);
+            return (THD);
         }
     }
 
-    plotData (spectrum, element) {
+    calcTHDN(data, fundementalIndex) {
+
+        var SumOfAmplitudes = 0;
+        //Start after the DC bin
+        for (var i = 1; i < data.length; i++) {
+            if (i != fundementalIndex && i != fundementalIndex + 1 && i != fundementalIndex - 1) {
+                SumOfAmplitudes = SumOfAmplitudes + Math.pow(data[i], 2)
+            }
+        }
+
+        var THDN = Math.sqrt(SumOfAmplitudes);
+
+        THDN = THDN / data[fundementalIndex];
+
+        THDN = 20 * Math.log10(THDN)
+
+
+        if (THDN == -300) {
+            console.log("THDN Calculation Failed");
+        }
+        else {
+            console.log("THDN: " + THDN);
+            return (THDN);
+        }
+    }
+
+    calcSNR(THDN, fundemental) {
+        return (THDN / fundemental);
+    }
+
+    plotData(spectrum, element) {
         var x = [];
-        
-        for (var i = 0; i < spectrum.length-1; i++) {
+
+        for (var i = 0; i < spectrum.length - 1; i++) {
             x.push(i);
         }
-        
+
 
         var trace = {
             type: 'scatter',                    // set the chart type
@@ -212,12 +263,12 @@ class dataModel {
             x: x,
             y: spectrum,
             line: {                             // set the width of the line.
-              width: 2,
-              color: 'rgb(71,23,246)'
+                width: 2,
+                color: 'rgb(71,23,246)'
             }
-          };
-      
-          var layout = {
+        };
+
+        var layout = {
             yaxis: {
                 title: "dB",
                 color: 'rgb(231,223,221)'
@@ -232,9 +283,9 @@ class dataModel {
             },
             paper_bgcolor: 'rgba(0,0,0,0)',
             plot_bgcolor: 'rgba(0,0,0,0)'
-          };
-      
-          Plotly.plot(document.getElementById(element), [trace], layout, {showLink: false});
+        };
+
+        Plotly.plot(document.getElementById(element), [trace], layout, { showLink: false });
 
         /*
         var TESTER = document.getElementById(element);
@@ -249,11 +300,11 @@ class dataModel {
         } );*/
     }
 
-    plotData2 (decibels) {
+    plotData2(decibels) {
 
         var x = [];
-        
-        for (var i = 0; i < decibels.length-1; i++) {
+
+        for (var i = 0; i < decibels.length - 1; i++) {
             x.push(i);
         }
     }
